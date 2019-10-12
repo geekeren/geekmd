81ba697cf3730c262416bd0c2073b19623557598
Baiyuan Wang <bywang@thoughtworks.com>
2019-10-12 20:18:42 +0800

    Updates

---
+++ precache-manifest.ea42d646030a1f909788eb8e8aad0db7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "53fe7c18c04f127635791f22c83200d1",
+    "url": "/data/example.md"
+  },
+  {
+    "revision": "dd90fe11cc301bdd99ed283526fa1858",
+    "url": "/themes/tech-radar/images/header.png"
+  },
+  {
+    "revision": "38c9a105217bee2d4641",
+    "url": "/js/chunk-vendors.22ba37ba.js"
+  },
+  {
+    "revision": "fe96eb7dafd2e01d4bf1833ff0c5d941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b26cd83298b3e523166e16bb48cdfd",
+    "url": "/themes/tech-radar/style.css"
+  },
+  {
+    "revision": "727a29d801734346ffcf",
+    "url": "/js/app.510b7e5c.js"
+  },
+  {
+    "revision": "f17636156bbecdc915da99347a9e4c9e",
+    "url": "/themes/basic.css"
+  },
+  {
+    "revision": "47558b3836cd87680c830e41a47da285",
+    "url": "/themes/tech-radar/images/h2-bg.jpeg"
+  },
+  {
+    "revision": "0c8675544ea7e6c3d0aeb914215fd393",
+    "url": "/themes/geedit.css"
+  },
+  {
+    "revision": "38c9a105217bee2d4641",
+    "url": "/css/chunk-vendors.7f0ef291.css"
+  },
+  {
+    "revision": "727a29d801734346ffcf",
+    "url": "/css/app.3d4bf20e.css"
+  },
+  {
+    "revision": "9eca2a95fe945fa6951c33211656ef57",
+    "url": "/CNAME"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.15699048f149b9c4834dbc6900948dbf.js"
+  "/precache-manifest.ea42d646030a1f909788eb8e8aad0db7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "geedit"});
