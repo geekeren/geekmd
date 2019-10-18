17623242ebe1119fe4b30ba2ca95a49b9cd073a8
Baiyuan Wang <bywang@thoughtworks.com>
2019-10-18 14:29:23 +0800

    Updates

--- precache-manifest.6546edc32e1ecb9015ea9007e43671de.js
+++ precache-manifest.6546edc32e1ecb9015ea9007e43671de.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "715dfa30c80c3b22422a6e5dd8805b54",
-    "url": "/themes/geedit.css"
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1",
+    "url": "/robots.txt"
   },
   {
     "revision": "12f0e31e1d500b030a8f04c7bb25efd0",
@@ -12,39 +12,39 @@ self.__precacheManifest = [
     "url": "/themes/tech-radar/style.css"
   },
   {
-    "revision": "788e5760698e4f06d513",
-    "url": "/css/chunk-vendors.7f0ef291.css"
+    "revision": "067ba738fec93733d4fce55106bf51d7",
+    "url": "/themes/basic.css"
   },
   {
-    "revision": "788e5760698e4f06d513",
-    "url": "/js/chunk-vendors.b72d4c12.js"
+    "revision": "7a8fdb90527f9cac1b83",
+    "url": "/js/auth.4211faa0.js"
   },
   {
     "revision": "dd90fe11cc301bdd99ed283526fa1858",
     "url": "/themes/tech-radar/images/header.png"
   },
   {
-    "revision": "5e0bd1c281a62a380d7a948085bfe2d1",
-    "url": "/robots.txt"
-  },
-  {
     "revision": "c8505026facf88b5c8862ddf276d84e2",
     "url": "/themes/tech-radar/images/left-border-bg.png"
   },
   {
-    "revision": "067ba738fec93733d4fce55106bf51d7",
-    "url": "/themes/basic.css"
+    "revision": "f83195ae8e31c2e95ffb",
+    "url": "/js/chunk-vendors.4955034c.js"
   },
   {
     "revision": "47558b3836cd87680c830e41a47da285",
     "url": "/themes/tech-radar/images/h2-bg.jpeg"
   },
   {
-    "revision": "85ba5bf35e041f385251",
-    "url": "/js/app.8ccc781c.js"
+    "revision": "715dfa30c80c3b22422a6e5dd8805b54",
+    "url": "/themes/geedit.css"
   },
   {
-    "revision": "3308e4d8df075f20833b541fc3dbcceb",
+    "revision": "d5b4c4b45c84bdb6bcb8",
+    "url": "/js/index.404fbb4a.js"
+  },
+  {
+    "revision": "47cf1eb3296c4bbaa49075dbc5450d95",
     "url": "/index.html"
   },
   {
@@ -52,8 +52,16 @@ self.__precacheManifest = [
     "url": "/data/example.md"
   },
   {
-    "revision": "85ba5bf35e041f385251",
-    "url": "/css/app.b8fdca5e.css"
+    "revision": "d5b4c4b45c84bdb6bcb8",
+    "url": "/css/index.5ef860da.css"
+  },
+  {
+    "revision": "f83195ae8e31c2e95ffb",
+    "url": "/css/chunk-vendors.7f0ef291.css"
+  },
+  {
+    "revision": "0e56c5a94fe97e16cc0bc6602a7785cc",
+    "url": "/auth/index.html"
   },
   {
     "revision": "9eca2a95fe945fa6951c33211656ef57",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1578b1f598f0e623220ae0707a4ffba1.js"
+  "/precache-manifest.6546edc32e1ecb9015ea9007e43671de.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "geedit"});
